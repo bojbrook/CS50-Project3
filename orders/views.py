9dba22f8e88f6349a4e999518dc6a62a11d20ddb
Bowen Brooks <bowenbrooks@Bowens-MacBook-Pro-2.local>
2019-05-22 11:01:42 -0700

    added template for shopping cart

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,14 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Toppings, Food, dinnerTypes
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+          return render(request, "orders/login.html", {"message": None})
     context = {
         "toppings": Toppings.objects.all(),
         "foods": Food.objects.all(),
@@ -18,3 +22,20 @@ def index(request):
     }
     return render(request,"orders/index.html", context)
     
+
+def login_view(request):
+      username = request.POST["username"]
+      password = request.POST["password"]
+      user = authenticate(request, username=username, password=password)
+      if user is not None:
+          login(request, user)
+          return HttpResponseRedirect(reverse("index"))
+      else:
+          return render(request, "users/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+      logout(request)
+      return render(request, "orders/login.html", {"message": "Logged out."})
+
+def cart_view(request):
+    return render(request,"orders/cart.html")
